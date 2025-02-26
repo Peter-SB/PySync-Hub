17c1dda980cb981ed89fa8157f1a93ff043a065e
Peter-SB <peterboughton11@live.co.uk>
2025-02-26 21:12:33 +0000

    Platform Icon

--- frontend-react/src/components/PlaylistItem.js
+++ frontend-react/src/components/PlaylistItem.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import DownloadStatus from './DownloadStatus';
 
 function PlaylistItem({ playlist, refreshPlaylists, isSelected, onSelectChange }) {
   return (
@@ -16,18 +17,22 @@ function PlaylistItem({ playlist, refreshPlaylists, isSelected, onSelectChange }
         <img src={playlist.image_url} alt="Playlist cover" className="w-14 h-14 rounded-md object-cover mr-4" />
       )}
       <div className="flex-1">
-        <h3 className="font-medium text-gray-900">
+        <h3 className="font-medium text-gray-900 flex items-center">
           <a href={playlist.url} target="_blank" rel="noreferrer" className="hover:underline">
             {playlist.name}
           </a>
+          {playlist.platform === "spotify" && (
+            <img src="/icons/spotify.svg" alt="Spotify" className="w-5 h-5 ml-2" />
+          )}
+          {playlist.platform === "soundcloud" && (
+            <img src="/icons/soundcloud.svg" alt="SoundCloud" className="w-4 h-4 p-0.5 ml-2" />
+          )}
         </h3>
         <div className="text-sm text-gray-600">
           {playlist.last_synced ? `Last synced: ${new Date(playlist.last_synced).toLocaleString()}` : 'Not synced'}
         </div>
       </div>
-      <div id={`download-status-${playlist.id}`}>
-        {/* Additional controls (e.g., download progress) can be added here */}
-      </div>
+      <DownloadStatus playlist={playlist} refreshPlaylists={refreshPlaylists} />
     </div>
   );
 }
